bbc310849f05cde870bda9d643f899b0db3b7260
2mol <2mol@users.noreply.github.com>
2021-07-07 18:54:08 +0200

    random, but it doesn't work yet

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@
 open System.Text
 
 open SharpVG
+open FsRandom
 
 let rectSize = 32
 let nRectsX = 12
@@ -9,31 +10,44 @@ let nRectsY = 24
 let nRects = nRectsX * nRectsY
 let margin = 1
 let canvasArea =
-    { Width = Length.ofInt (rectSize * 12 + 2 * margin)
-      Height = Length.ofInt (rectSize * 24 + 2 * margin)
-    }
+  { Width = Length.ofInt (rectSize * 12 + 2 * margin)
+    Height = Length.ofInt (rectSize * 24 + 2 * margin)
+  }
+
+let randomState = createState xorshift (123456789u, 362436069u, 521288629u, 88675123u)
 
 let rectangle n =
+  random {
+    let! offSetX = ``[0, 1)``
+    let! offSetY = ``[0, 1)``
+    // let! offSetY = Statistics.normal (0., 10. * (float n))
     let style = Style.create (Color.ofName White) (Color.ofName Black) (Length.ofInt 1) 1.0 0.0
-    let position = Point.ofInts (rectSize * (n % nRectsX) + margin, rectSize * (n / nRectsX) + margin)
+    let pureX = rectSize * (n % nRectsX) + margin |> float
+    let pureY = rectSize * (n / nRectsX) + margin |> float
+    let position = Point.ofFloats (pureX + (20.*offSetX), pureY + (20.*offSetY))
     let area = Area.ofInts (rectSize, rectSize)
-    Rect.create position area
-    |> Element.createWithStyle style
+
+    let element =
+        Rect.create position area
+        |> Element.createWithStyle style
+
+    return element
+  }
 
 [<EntryPoint>]
 let main _ =
-    let drawing =
-        {0 .. nRects - 1}
-        |> Seq.map rectangle
-        |> Group.ofSeq
-        // |> Group.addElements
-        |> Svg.ofGroup
-        |> Svg.withSize canvasArea
+  let drawing =
+    {0 .. nRects - 1}
+    |> Seq.map (fun n -> Random.get (rectangle n) randomState)
+    |> Group.ofSeq
+    // |> Group.addElements
+    |> Svg.ofGroup
+    |> Svg.withSize canvasArea
 
-    // let html =
-    //     Svg.toHtml "Drawing" drawing
+  // let html =
+  //     Svg.toHtml "Drawing" drawing
 
-    // File.WriteAllText("index.html", html, Encoding.UTF8)
-    File.WriteAllText("out.svg", drawing.ToString(), Encoding.UTF8)
+  // File.WriteAllText("index.html", html, Encoding.UTF8)
+  File.WriteAllText("out.svg", drawing.ToString(), Encoding.UTF8)
 
-    0
+  0
