4506ca47f27359833f67d89cdeac088d1ca130b0
2mol <2mol@users.noreply.github.com>
2021-07-07 19:09:24 +0200

    random, but it works

--- Program.fs
+++ Program.fs
@@ -2,7 +2,6 @@
 open System.Text
 
 open SharpVG
-open FsRandom
 
 let rectSize = 32
 let nRectsX = 12
@@ -10,35 +9,30 @@ let nRectsY = 24
 let nRects = nRectsX * nRectsY
 let margin = 1
 let canvasArea =
-  { Width = Length.ofInt (rectSize * 12 + 2 * margin)
-    Height = Length.ofInt (rectSize * 24 + 2 * margin)
+  { Width = Length.ofInt (rectSize * 12 + 2 * margin + 30)
+    Height = Length.ofInt (rectSize * 24 + 2 * margin + 30)
   }
 
-let randomState = createState xorshift (123456789u, 362436069u, 521288629u, 88675123u)
+let random = new System.Random(1)
 
 let rectangle n =
-  random {
-    let! offSetX = ``[0, 1)``
-    let! offSetY = ``[0, 1)``
-    // let! offSetY = Statistics.normal (0., 10. * (float n))
-    let style = Style.create (Color.ofName White) (Color.ofName Black) (Length.ofInt 1) 1.0 0.0
-    let pureX = rectSize * (n % nRectsX) + margin |> float
-    let pureY = rectSize * (n / nRectsX) + margin |> float
-    let position = Point.ofFloats (pureX + (20.*offSetX), pureY + (20.*offSetY))
-    let area = Area.ofInts (rectSize, rectSize)
+  let offSetX = float (random.Next(0,n)) / 12.
+  let offSetY = float (random.Next(0,n)) / 12.
+  let style = Style.create (Color.ofName White) (Color.ofName Black) (Length.ofInt 1) 1.0 0.0
+  let pureX = rectSize * (n % nRectsX) + margin
+  let pureY = rectSize * (n / nRectsX) + margin
+  let position = Point.ofFloats (float pureX + offSetX, float pureY + offSetY)
+  let area = Area.ofInts (rectSize, rectSize)
 
-    let element =
-        Rect.create position area
-        |> Element.createWithStyle style
+  Rect.create position area
+  |> Element.createWithStyle style
 
-    return element
-  }
 
 [<EntryPoint>]
 let main _ =
   let drawing =
     {0 .. nRects - 1}
-    |> Seq.map (fun n -> Random.get (rectangle n) randomState)
+    |> Seq.map rectangle
     |> Group.ofSeq
     // |> Group.addElements
     |> Svg.ofGroup
